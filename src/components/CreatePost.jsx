d1b438663cebfcd6d72f73233f4ac4a0be90ea20
Nevin10son <nevintenson235@gmail.com>
2024-07-28 23:11:35 +0530

    created

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CreatePost = () => {
+    const [input,setInput]=new useState(
+        {"userId":sessionStorage.getItem("userId"),"Message":""}
+      )
+      const inputHandler=(event)=>{
+        setInput({...input,[event.target.name]:event.target.value})
+      }
   return (
     <div>
-      hello
+      
     </div>
   )
 }
